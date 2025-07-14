242ba84b9270e2d1d5f0efaecb62447b7f6a4e7d
GianmarcoBasile <basilemarc2000@gmail.com>
2025-07-14 20:00:29 +0200

    fix: stop scripts and deploy pipeline

--- stop_instance_script.bat
+++ stop_instance_script.bat
@@ -5,8 +5,4 @@ for %%R in (master workers) do (
 
     aws ec2 stop-instances --instance-ids %INSTANCE_ID% --region eu-west-2
     echo Istanza EC2 %INSTANCE_ID% stoppata
-
-    aws ec2 disassociate-address --allocation-id %ALLOCATION_ID% --region eu-west-2
-    aws ec2 release-address --allocation-id %ALLOCATION_ID% --region eu-west-2
-    echo Elastic IP %ALLOCATION_ID% disassociato e rilasciato
 )
--- stop_instance_script.sh
+++ stop_instance_script.sh
@@ -6,8 +6,4 @@ for role in master workers; do
 
     aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region eu-west-2
     echo "Istance EC2 $INSTANCE_ID stoppata"
-
-    aws ec2 disassociate-address --allocation-id "$ALLOCATION_ID" --region eu-west-2
-    aws ec2 release-address --allocation-id "$ALLOCATION_ID" --region eu-west-2
-    echo "Elastic IP $ALLOCATION_ID disassociato e rilasciato"
 done
